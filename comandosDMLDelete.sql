a9578f6b2122f35ab6199ad9360a5f1011bdfcab
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-11-03 18:11:41 -0300

    comandos DML delete

---
+++ comandosDMLDelete.sql
@@ -0,0 +1,5 @@
+-- Utilizando o comando DELETE
+  -- Cuidado se você esquecer a clausa where todas as linhas da tabela serão deletadas
+
+delete from rodrigo_rodrigues.countries
+where country_name = 'Nigeria';
