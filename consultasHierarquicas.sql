70ccd8c745a24887e6ca65fb010f2be023f1ef9e
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-11-10 18:55:09 -0300

    consultas up/down e bottom/up

--- consultasHierarquicas.sql
+++ consultasHierarquicas.sql
@@ -9,3 +9,25 @@ from rodrigo_rodrigues.employees
 start with manager_id is null
 -- Especifica se é bottom/up ou up/down
 connect by prior employee_id = manager_id;
+
+-- -/--------------------/-
+
+-- Outro exemplo de consulta up/down
+
+select level, employee_id, first_name, last_name, job_id, manager_id
+from rodrigo_rodrigues.employees
+-- Navega a partir do empregado 103
+start with employee_id = 103
+-- up/down
+-- connect by prior employee_id = manager_id;
+connect by manager_id = prior employee_id;
+
+-- -/--------------------/-
+
+select level, employee_id, first_name, last_name, job_id, manager_id
+from rodrigo_rodrigues.employees
+-- Navega a partir do empregado 103
+start with employee_id = 103
+-- up/down
+-- connect by prior employee_id = manager_id;
+connect by prior manager_id = employee_id;
